d1ad4d34a5cb47fce431acb9f95ae6e0ccb8225a
Liam Houghton <liamhoughton@192.168.1.131>
2024-07-19 00:06:57 +0100

    make search input fields responsive

--- src/Components/Header.jsx
+++
@@ -1,8 +0,0 @@
-import SearchBar from "./SearchBar";
-
-export default function Header() {
-    return <header>
-        <h1>Metrical</h1>
-        <SearchBar displayDirection={'horizontal'}/>
-    </header>
-}
